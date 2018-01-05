12d7adbc154260d566d002565ad9bf96c6eb82f6
mal90 <SalimM@synergenhealth.com>
2018-01-05 04:18:37 -0600

    Add shopping-list , ingredient , recipe-list

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+
+  recipes: Recipe[] = [
+    new Recipe('Recipe 1','This is recipe no. 1','https://imagesvc.timeincapp.com/v3/mm/image?url=https%3A%2F%2Fcdn-image.realsimple.com%2Fsites%2Fdefault%2Ffiles%2Fstyles%2Fportrait_435x518%2Fpublic%2Fimage%2Fimages%2Ffood-recipes%2Frecipe-collections%2F0804%2Fmini-burger_300.jpg%3Fitok%3DOWhS6WbV&w=700&q=85'),
+    new Recipe('Recipe 1','This is recipe no. 1','https://imagesvc.timeincapp.com/v3/mm/image?url=https%3A%2F%2Fcdn-image.realsimple.com%2Fsites%2Fdefault%2Ffiles%2Fstyles%2Fportrait_435x518%2Fpublic%2Fimage%2Fimages%2Ffood-recipes%2Frecipe-collections%2F0804%2Fmini-burger_300.jpg%3Fitok%3DOWhS6WbV&w=700&q=85')
+  ]    
 
   constructor() { }
 
