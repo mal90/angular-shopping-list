e9a029af04cdf985fe8d6a2211b065f0e3ce1a01
mal90 <SalimM@synergenhealth.com>
2018-01-22 01:50:00 -0600

    Add shopping list service

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,6 +1,11 @@
+import {EventEmitter } from '@angular/core';
 import { Recipe } from "./recipe.model";
 
+
 export class RecipeService{
+
+    recipeSelected = new EventEmitter<Recipe>();
+
     private recipes: Recipe[] = [
         new Recipe('A Test Recipe', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg'),
         new Recipe('A Test Recipe', 'This is another test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg')
@@ -9,4 +14,6 @@ export class RecipeService{
     getRecipes() {
         return this.recipes.slice();
     }
+
+
 }
